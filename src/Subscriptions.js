cc072d61c654bae65adc93301f79a53b891b5088
renan <renanbazinin2@gmail.com>
2022-06-17 14:11:22 +0300

    first commit

---
+++ src/Subscriptions.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import { Routes, Route, Link ,useNavigate, Outlet} from "react-router-dom"
+
+export default function Subscriptions() {
+  const navigate = useNavigate()
+  useEffect(() => {
+    
+    // some 
+    if(!sessionStorage.getItem("loged")){
+      navigate("/login")
+      return
+    }
+    navigate("allmembers")
+
+
+}, [])
+
+const naviHandle = (e)=>{
+  console.log("try path")
+  const path = e.target.name
+  navigate(path)
+}
+
+  return <div>
+    
+  <h2>Subscriptions</h2>
+  <button onClick={naviHandle} name="allmembers">All members</button>
+    <button onClick={naviHandle} name="addmember">Add member</button>
+  <Outlet />
+  </div>;
+}
